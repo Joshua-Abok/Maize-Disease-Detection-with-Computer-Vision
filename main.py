f34ee133b9ebbb8978bd36e736b3f035ed34bd00
Joshua Abok <nyajuaya.j.a@gmail.com>
2023-10-29 12:06:57 +0300

    [2] preparing base model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.maizeDiseaseDetection import logger
 # logger.info("Welcome to our custom log 2")
 
 from src.maizeDiseaseDetection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.maizeDiseaseDetection.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -12,6 +13,18 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx=========x")
+except Exception as e: 
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare Base Model"
+
+try: 
+    logger.info(f"***********************")
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed  <<<<<<<\n\nx=============x")
 except Exception as e: 
     logger.exception(e)
     raise e
--- src/maizeDiseaseDetection/entity/config_entity.py
+++ src/maizeDiseaseDetection/entity/config_entity.py
@@ -10,3 +10,16 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path                       # everything written in config.yaml file
+
+
+# update entity on part 2: prepare_base_model
+@dataclass(frozen=True)    # frozen=True -> don't want any changes
+class PrepareBaseModelConfig: 
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool 
+    params_weights: str
+    params_classes: int
